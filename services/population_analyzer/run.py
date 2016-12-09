dc636b59d3e3ef7e938b89597a27423065015b7f
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-12-08 18:19:29 -0800

    Added more queues

---
+++ services/population_analyzer/run.py
@@ -0,0 +1,20 @@
+from os import environ
+from multiprocessing import current_process
+
+import rsyslog
+from redis import StrictRedis, ConnectionPool
+from rq import Worker, Queue, Connection
+
+# IMPORT HERE ANY PACKAGES THE QUEUE WILL NEED
+
+current_process().name = environ['HOSTNAME']
+pool = ConnectionPool(host='redis', max_connections=1)
+
+with Connection(Redis(connection_pool = pool)):
+    queue = Queue(environ['HOSTNAME'])
+    worker = Worker(queue)
+    current_process().name = environ['HOSTNAME'] + worker.name[0:5]
+    worker.work(logging_level = environ['LOG_LEVEL'])
+    return 0
+
+return 1
