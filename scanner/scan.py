c9f6934a6691e0f35c811752b9e688386e07730c
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-12-22 16:37:10 -0800

    Added optional block on github_crawler job

--- scanner/scan.py
+++ scanner/scan.py
@@ -1,16 +1,10 @@
-import re
 import os
-import time
 import json
-import pickle
-import bisect
 import rsyslog
 import logging
-import mimetypes
 import functools
 
 import boto3
-import psycopg2
 import botocore
 
 from githubcrawler import GithubCommitCrawler
@@ -32,7 +26,7 @@ BUCKET = os.environ['S3_BUCKET']
 LEADERBOARD_PREFIX = os.environ['S3_LEADERBOARD_PREFIX']
 USER_PREFIX = os.environ['S3_USER_PREFIX']
 
-def scan_all_repos(access_token, skill_set_id):
+def scan_all_repos(access_token, block_until_consistent = True):
     user = DeveloperProfile()
     crawler = GithubCommitCrawler(access_token, user.analyze_commit, TMPFS_DRIVE, LARGE_DRIVE, TMPFS_MAX_WRITE)
     github_id = crawler.api.get_user().login
@@ -43,7 +37,11 @@ def scan_all_repos(access_token, skill_set_id):
         return compute_knowledge(bucket, language, module, github_id, dates, user.knowledge_activation)
 
     knowledge = user.compute_knowledge(_compute_knowledge)
-    bucket.Object('{}/{}'.format(USER_PREFIX, github_id)).put(Body = json.dumps(knowledge))
+    knowledge_object = bucket.Object('{}/{}'.format(USER_PREFIX, github_id))
+    etag = knowledge_object.put(Body = json.dumps(knowledge))['ETag']
+
+    if block_until_consistent:
+        knowledge_object.wait_until_exists(IfMatch = etag)
 
 def compute_knowledge(bucket, language, module, github_id, dates, knowledge_activation_function):
     knowledge = functools.reduce(lambda prev, curr: prev + knowledge_activation_function(curr), dates, 0.0)
@@ -53,17 +51,3 @@ def compute_knowledge(bucket, language, module, github_id, dates, knowledge_acti
     bucket.Object(key = '{}:{:.2f}'.format(key, knowledge)).put(Body = bytes('', 'utf-8'))
 
     return knowledge
-
-def write_rankings_to_db(skill_set_id, rankings):
-    try:
-        connection = psycopg2.connect(dbname = 'postgres', user = 'postgres', password = '', host = 'database')
-        cursor = connection.cursor()
-        cursor.execute('UPDATE skill_set SET skills=(%s) WHERE id=(%s)', (pickle.dumps(rankings), skill_set_id))
-    except Exception as exc:
-        LOGGER.exception('Couldnt write skills to database!')
-    finally:
-        try:
-            cursor.close()
-            connection.close()
-        except:
-            pass
